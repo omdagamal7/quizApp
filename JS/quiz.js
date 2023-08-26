bcf8815c14adfd2aea33c01a8fef3989d33fb117
Emad Jammal <emad.elqassas.1@gmail.com>
2023-08-26 15:41:51 +0300

    quiz app

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,43 +1,49 @@
 export class Quiz {
   constructor(results){
 
-    // API VALUE
+    // API DATA FROM SETTING FILE
     this.results = results;
 
-    console.log(this.results);
-    // QUESTIONS COUNTER
-    $("#to").html(this.results.length)
 
+    // SET QUESTIONS COUNT IN HTML
+    $("#to").html(this.results.length)
+    // SELECT QUESTION TITLE ELEMENT
     this.questionTitle = document.getElementById("questionTitle")
+    // VARIABLE FOR COUNTING QUESTIONS
     this.currentIndex = 0;
+    // CORECT ANSWER FROM USE OUTSIDE CONSTRUCTOR
     this.correctAnswer;
+
+    // * RELOADING END PAGE FOR RESTART GAME
     $("#end").click(()=>{
       location.reload();
     })
+    // FOR COUNTING CORRECT ANSWERS
     this.score = 0;
+    // RUN SHOW QUESTION FUNCTION
     this.showQuestion();
   }
   showQuestion(){
+    //  SET CURRENT QUESTION COUNT IN HTML
       $("#from").html(this.currentIndex + 1);
-
+      // CURRENT QUESTION
       const currentQuestion = this.results[this.currentIndex].question;
-
+      // SET CURRENT QUESTION IN HTML
       this.questionTitle.innerHTML = currentQuestion;
-
+      // FOR GET THE CORRECT ANSWER FROM API DATA
       this.correctAnswer = this.results[this.currentIndex].correct_answer;
-
+      // FOR GET THE ARRAY OF INCORRECT ANSWER FROM API DATA
       const answers = this.results[this.currentIndex].incorrect_answers;
-
+      // GENERATE RANDOM NUMBER FROM 0 TO 3
       const randomNumber = Math.round(Math.random()*answers.length);
-
+      // FOR ADD CORRECT ANSWER BETWEEN INCORRECT ANSWERS IN RANDOM INDEX
       answers.splice(randomNumber,0,this.correctAnswer);
-
+      //! RUN THE NEXT QUESTION FUNCTION AS CLICK ON NEXT
       $("#nextQuestion").click(()=>{
         this.nextQuestion()
       })
-
+      // FOR SET ANSWERS IN HTML
       let option = '';
-
       for (let i = 0; i < answers.length; i++) {
         option += `<li class="my-3 animate__animated">
         <div class="pretty p-default p-round p-smooth p-plain">
@@ -47,31 +53,48 @@ export class Quiz {
       </li>`
         
       }
-
       $("#questionContent").html(option)
     }
+    // ! NEXT QUESTION
   nextQuestion(){
-    const answerChecked = document.querySelector("[name='answer']:checked").value
+    // GET THE ANSWER IS CHECKED
+    const answerChecked = document.querySelector("[name='answer']:checked").value;
+
     if (answerChecked == undefined) {
+      // SHOWING THIS ALERT IF NOT CHECKED VALUE
       $("#alertAns").fadeIn(300);
 
     } else {
+      // IF CHECKED VALUE CURRENT INDEX + 1
       this.currentIndex++;
+      // IF CHECKED VALUE HIDE ALERT
       $("#alertAns").fadeOut(300);
+
       if (this.currentIndex > this.results.length - 1) {
+        // HIDE QUIZ PAGE
         $("#quiz").removeClass("show");
+        // SHOW QUIZ SCORE
         $("#finish").addClass("show");
-        $("#score").html(this.score)
+        // SET SCORE IN HTML
+        $("#score").html(this.score);
+
       } else {
+
         if (answerChecked == this.correctAnswer) {
-          $("#inCorrect").fadeOut(0)
-          $("#correct").fadeIn(800)
+          // HIDE ALERT INCOTTECT
+          $("#inCorrect").fadeOut(0);
+          // SHOW ALERT CORRECT
+          $("#correct").fadeIn(800);
+          // SCORE + 1
           this.score++;
-          console.log(this.score);
         } else {
-          $("#correct").fadeOut(0)
-          $("#inCorrect").fadeIn(800)
+          // HIDE ALERT CORRECT
+          $("#correct").fadeOut(0);
+          // SHOW ALERT INCORRECT
+          $("#inCorrect").fadeIn(800);
         }
+
+        // AND RUN SHOW QUESTION FUNCTION
         this.showQuestion();
       }
     }
--- JS/setting.js
+++ JS/setting.js
@@ -1,27 +1,42 @@
+// IMPORT CLASS FROM QUIZ
 import { Quiz } from "./quiz.js";
 export class Setting {
   constructor (){
+    // RUN CATEGORY FUNCTION
     $("#start").click(()=>{
       this.startCategory()
     })
   }
+  
   async startCategory(){
+    // CATEGORY VALUES
     const category =  $("#category").val();
+    // DIFFICULTY VALUES
     const difficulty = Array.from($("input[name=difficulty]")).find((item) =>item.checked).value;
+    // AMOUNT VALUES
     const amount = $("#amount").val()
+
     if (amount < 1) {
       $("#alertNumber").addClass("show")
     }
     else if (amount > 0){
       $("#alertNumber").removeClass("show")
+      //! RUN GET QUESTION FUNCTION AND SEND ARGUMENTS (AMOUNT VALUES, DIFFICULTY VALUES, CATEGORY VALUES)
       const results = await this.getQuestions(category,difficulty,amount);
+
       $("#setting").removeClass("show");
       $("#quiz").addClass("show");
+
+      // SEND API DATA TO QUIZ CLASS IN QUIZ FILE
       const quiz = new Quiz(results);
+
     }
   }
+  // ! CATEGORY FUNCTION
   async getQuestions(category,difficulty,amount){
+    // GET DATA FROM API
     const apiResponse = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`)
+    // PARSE API DATA
     const apiData = await apiResponse.json();
     return apiData.results;
   }
